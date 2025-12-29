f914c31076c39278b1110aa8ea12496ffaa277f3
izyannn0 <izyanathirahhh@student.usm.my>
2025-12-29 21:42:45 +0800

    finish

--- Drink/Display_DrinkMenu.cpp
+++ Drink/Display_DrinkMenu.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+#include "display_DrinkMenu.hpp"
+
 using namespace std;
 
 void display_DrinkMenu() {
-    cout << "Drink Menu\n";
+    cout << "\n===== DRINK MENU =====\n";
+    cout << "1. Mineral Water  - RM2.00\n";
+    cout << "2. Coca Cola      - RM3.50\n";
+    cout << "3. Orange Juice   - RM4.00\n";
+    cout << "4. Teh Ais        - RM2.50\n";
+    cout << "5. Coffee         - RM3.00\n";
+    cout << "======================\n";
 }
